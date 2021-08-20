ec610b14d7a22de4cf1003c2c12acf6e3a6b562f
Anurag Prasad <anuragp0010@gmail.com>
2021-08-20 15:15:26 +0530

    Updated

---
+++ src/main/java/com/springrest/practice/springrestpractice/SpringrestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.practice.springrestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestPracticeApplication.class, args);
+	}
+
+}
